e8940bf27fa7cc33c38022e938891dac75687914
Остапенко Андрей <ostapenko_av@ascon.local>
2024-11-25 20:22:16 +0700

    changed content-types for req and res for /urls/create, added cors header

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -1,33 +1,49 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"regexp"
 )
 
 func validateUrl(url string) bool {
-	// validUrl := regexp.MustCompile(`(http(s)?:\/\/)?(www\.)?([a-zA-Z]+\.)+[a-zA-Z]{2,6}(\/[\w?=#-$.]+)+`)
 	validUrl := regexp.MustCompile(`^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$`)
 	isValid := validUrl.MatchString(url)
 	return isValid
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func createUrl(w http.ResponseWriter, r *http.Request) {
-	fullUrl := r.FormValue("fullUrl")
-
-	if !validateUrl(fullUrl) {
-		w.Write([]byte("url not valid"))
-		return
+	type reqJson struct {
+		FullUrl string
 	}
-
-	shortenedUrl, err := dbConnection.CreateUrl(fullUrl, GetCurrentServer().host)
+	enableCors(&w)
+	var j reqJson
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&j)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = w.Write([]byte(shortenedUrl))
+	if !validateUrl(j.FullUrl) {
+		w.Write([]byte(fmt.Sprintf("url %s not valid", j.FullUrl)))
+		return
+	}
+
+	hash, err := dbConnection.CreateUrl(j.FullUrl, GetCurrentServer().host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	_, err = w.Write([]byte(fmt.Sprintf("{\"shortenedUrl\": \"%s:%s/%s\"}", GetCurrentServer().host, GetCurrentServer().port, hash)))
 
 	if err != nil {
 		log.Fatal(err)
@@ -43,6 +59,7 @@ func getAllUrls(w http.ResponseWriter, r *http.Request) {
 }
 
 func redirectToOriginalUrl(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	path := r.URL.Path
 	fullUrl, err := dbConnection.GetFullUrl(path[1:])
 
